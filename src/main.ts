715e2215da0589c018be7208e77304cd7aaaecdf
James Michael Kuizon <jameskuizon13@gmail.com>
2022-06-12 23:48:18 +0800

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Parking Allocation System')
+    .setDescription('A parking allocation system for Object-oriented Mall')
+    .setVersion('1.0')
+    .addTag('parking-allocation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
