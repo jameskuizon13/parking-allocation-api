05ac24e556742dc6d1a6c77b78c444f832baa4df
James Michael Kuizon <jameskuizon13@gmail.com>
2022-06-14 13:20:24 +0800

    Added seed for parking slot

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,28 @@
-import { PrismaClient } from '@prisma/client';
+import { ParkingType, PrismaClient } from '@prisma/client';
 
 const entrances = ['A', 'B', 'C'];
+
+const parkingSlotTypes = [
+  { type: ParkingType.SP, price: 20 },
+  { type: ParkingType.MP, price: 60 },
+  { type: ParkingType.LP, price: 100 },
+];
 
 const prisma = new PrismaClient();
 
 async function main() {
   for (const entrance of entrances) {
     await prisma.parkingEntrance.create({ data: { name: entrance } });
+  }
+
+  for (let i = 0; i < parkingSlotTypes.length; i++) {
+    const { id } = await prisma.parkingSlotType.create({
+      data: parkingSlotTypes[i],
+    });
+
+    await prisma.parkingSlot.create({
+      data: { name: `P${i + 1}`, parkingSlotTypeId: id },
+    });
   }
 }
 
